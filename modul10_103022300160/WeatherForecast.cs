42632189984662f7d7a9628b54c90deb9a9a2a8d
Alyaras <alyarahmadayanis@gmail.com>
2025-04-28 00:14:26 +0700

    commit pertama

--- modul10_103022300160/WeatherForecast.cs
+++ modul10_103022300160/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace modul10_103022300160
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 30 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
